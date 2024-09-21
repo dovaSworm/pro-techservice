57e2bf72d61a524bdfa1628beaaf9af85f634bbf
dova <danijeldoveden.55@gmail.com>
2024-09-21 18:59:19 +0200

    prvi sa novog

--- index.php
+++ index.php
@@ -35,8 +35,7 @@ include 'header.php';
                     <a href="services.php#network">Pročitaj više</a>
                 </div>
             </div>
-        </div>
-        <div class="row g-0">
+
             <div class="col-12 col-md-6 col-lg-3 p-1">
                 <div class="serv-card recover-card">
                     <i class="fas fa-hdd d-block"></i>
@@ -71,19 +70,37 @@ include 'header.php';
 <section id="about">
     <div class="container">
         <h2>Zašto izabrati nas?</h2>
+        <div class="about-holder">
+            <div class="about-img-holder">
+                <img src="img/lap21.jpg" alt="protechnology-servis">
+            </div>
+        </div>
         <div class="row g-0 align-items-center">
-            <div class="col-sm-12 col-md-6 about-img-holder">
+            <div class="col-sm-12 col-md-9 m-auto ">
                 <img src="img/lap21.jpg" alt="protechnology-servis">
             </div>
             <div class="col-sm-12 col-md-6 about-right">
                 <h3 class="section-heading">Stručnost, kvalitet, efikasnost i ljubaznost</h3>
-                <p class="">Ovo je moto kojim se Pro techonology team rukovodi u nastupu prema klijentima koji dolaze u naš servis i ukazuju nam svoje poverenje. Zadovoljstvo klijenata našom kompletnom uslugom, počevši od prijema, preko rešavanja problema pa sve do predaje uređaja, predstavlja osnov našeg poslovanja. Naši iskusni serviseri nude stručnu i kvalitetnu uslugu, a efikasnost i ljubaznost se podrazumevaju za sve članove našeg tima koji su tu da bi pomogli u rešavanju svakog vašeg problema sa računarom. Zadovoljan klijent je najbolja potvrda našeg uloženog truda i najbolja preporuka za buduće korisnike naših usluga.</p>
+                <p class="">Ovo je moto kojim se Pro techonology team rukovodi u nastupu prema klijentima koji dolaze u
+                    naš servis i ukazuju nam svoje poverenje. Zadovoljstvo klijenata našom kompletnom uslugom, počevši
+                    od prijema, preko rešavanja problema pa sve do predaje uređaja, predstavlja osnov našeg poslovanja.
+                    Naši iskusni serviseri nude stručnu i kvalitetnu uslugu, a efikasnost i ljubaznost se podrazumevaju
+                    za sve članove našeg tima koji su tu da bi pomogli u rešavanju svakog vašeg problema sa računarom.
+                    Zadovoljan klijent je najbolja potvrda našeg uloženog truda i najbolja preporuka za buduće korisnike
+                    naših usluga.</p>
             </div>
         </div>
         <div class="row g-0 align-items-center">
             <div class="col-sm-12 col-md-6  about-left">
                 <h3 class="section-heading">Pro-technology servis nudi usluge fizičkim i pravinim licima</h3>
-                <p class="">Naš servis u saradnji sa partnerima nudi kompanijama mogućnost dogovora oko postavljanja i održavanja servera i mreže. Instaliranje legalnih sistema, umrežavanje računara i pravljenje baze podataka naš tim vrši prema zahtevima vaše kompanije, a uz to je i u pripravnosti da pomogne u situacijama kada dođe do nepredviđenih problema. Brigu o ispravnosti vaših sistema možete da prepustite pouzdanom timu koji će ih održavati u punoj funkciji redovnim kontrolama i servisima. Naš tim čine dugogodišnji profesionalci, iskusni serviseri koji mogu da savladaju svaki izazov koji se stavi pred njih. Na nas se možete s punim poverenjem osloniti po svim pitanjima koja se odnose na servisiranje računara ili usluga koje nudimo pravnim licima.</p>
+                <p class="">Naš servis u saradnji sa partnerima nudi kompanijama mogućnost dogovora oko postavljanja i
+                    održavanja servera i mreže. Instaliranje legalnih sistema, umrežavanje računara i pravljenje baze
+                    podataka naš tim vrši prema zahtevima vaše kompanije, a uz to je i u pripravnosti da pomogne u
+                    situacijama kada dođe do nepredviđenih problema. Brigu o ispravnosti vaših sistema možete da
+                    prepustite pouzdanom timu koji će ih održavati u punoj funkciji redovnim kontrolama i servisima. Naš
+                    tim čine dugogodišnji profesionalci, iskusni serviseri koji mogu da savladaju svaki izazov koji se
+                    stavi pred njih. Na nas se možete s punim poverenjem osloniti po svim pitanjima koja se odnose na
+                    servisiranje računara ili usluga koje nudimo pravnim licima.</p>
             </div>
             <div class="col-sm-12 col-md-6 img-fix about-img-holder">
                 <img src="img/desk8.jpg" alt="protechnology-servis">
